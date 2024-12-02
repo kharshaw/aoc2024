fbc0391d5d786521540e3eb77248abd2d316d0c6
kharshaw <keith.harshaw@gmail.com>
2024-12-02 07:20:34 -0500

    day1 done.  day2 half done

---
+++ day01/part1/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Tasks.Dataflow;
+
+
+var input = File.ReadAllLines("../data/input.txt");
+
+var lista = new List<int>();
+var listb = new List<int>();
+
+foreach (var line in input)
+{
+    var split = line.Replace("   ", " ").Split();
+    var a = int.Parse(split[0]);
+    var b = int.Parse(split[1]);
+    lista.Add(a);
+    listb.Add(b);
+}
+
+lista.Sort();
+listb.Sort();
+
+var acc = 0;
+
+for (var i = 0; i < lista.Count; i++)
+{
+    acc += int.Abs((lista[i] - listb[i]));
+}
+
+Console.WriteLine($"Part1: {acc}");
+
+acc = 0;
+
+lista.ForEach(a => {
+    acc += a * listb.FindAll(b => b == a).Count() ;
+});
+
+Console.WriteLine($"Part2: {acc}");
---
+++ day02/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+var input = File.ReadAllLines("data/input.txt");
+
+
+static bool isIncreasing(int a, int b) => a < b;
+
+var safeCount = 0;
+
+foreach (var report in input)
+{
+    var levels = report.Split().Select(int.Parse).ToList();
+
+    var levelsAreIncreasing = isIncreasing(levels[0], levels[1]);
+
+    var i = 0;
+    var isSafe = true;
+
+    while ((i < levels.Count - 1 ) && isSafe)
+    {
+        if ((levelsAreIncreasing && isIncreasing(levels[i], levels[i + 1])) || (!levelsAreIncreasing && !isIncreasing(levels[i], levels[i + 1])))
+        {
+            var delta = int.Abs(levels[i + 1] - levels[i]);
+
+            if (delta < 1 || delta > 3)
+            {
+                isSafe = false;
+            }
+        }
+        else
+        {
+            isSafe = false;
+        }
+        i++;
+
+    }
+
+    if (isSafe) safeCount++;
+}
+
+Console.WriteLine($"Part1: {safeCount}");
