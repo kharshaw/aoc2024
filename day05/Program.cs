22f4b99aa55b161ccd9c052787f26b7917232990
kharshaw <keith.harshaw@gmail.com>
2024-12-06 16:27:08 -0500

    days 5 and 6 working

--- day05/Program.cs
+++ day05/Program.cs
@@ -1,2 +1,114 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Net.NetworkInformation;
+
+Console.WriteLine("Hello, Day 5");
+
+var input = File.ReadAllLines("data/input.txt");
+
+var rules = new Dictionary<int, List<int>>();
+
+var i = 0;
+while (i < input.Length && input[i] != "")  
+{
+    var split = input[i].Split("|");
+    var key = int.Parse(split[0]);
+    var value = int.Parse(split[1]);
+    
+    if (!rules.ContainsKey(key)) rules[key] = new List<int>();
+    
+    rules[key].Add(value);
+
+    i++;
+}
+
+i++;
+
+var updates = new List<List<int>>();
+while (i < input.Length)
+{
+    var split = input[i].Split(",");
+    updates.Add(split.Select(int.Parse).ToList());   
+
+    i++;
+}
+
+// find updates ordered properly
+var ordered = new List<List<int>>();
+var unordered = new List<List<int>>();
+
+var isOrdered = true;
+foreach (var update in updates)
+{
+    for (var p =1; p < update.Count; p++)
+    {
+        var page = update[p];
+        if (rules.ContainsKey(page) && rules[page].Intersect(update.Take(p)).Any())
+        {
+            // rule violation
+            isOrdered = false;
+            break;
+        }
+    }
+    
+    if (isOrdered)
+    {
+        ordered.Add(update);
+    }
+    else
+    {
+        unordered.Add(update);
+    }
+    
+    isOrdered = true;
+}
+
+var acc = 0;
+
+foreach (var update in ordered)
+{
+    acc += update[update.Count/2 ];
+}
+Console.WriteLine($"Part1: {acc} {acc == 5129}");
+
+acc = 0;
+
+static bool CheckOrder(List<int> update, Dictionary<int, List<int>> rules)
+{
+    var isOrdered = true;
+
+    for (var p = 0; p < update.Count; p++)
+    {
+        var page = update[p];
+        if (rules.ContainsKey(page) && rules[page].Intersect(update.Take(p)).Any())
+        {
+            // take out the misordered page
+            update.Remove(page);
+
+            var others = rules[page].Intersect(update.Take(p));
+
+            update.Insert(update.IndexOf(others.First()), page);
+
+            isOrdered = false;
+        }
+    }
+
+    return isOrdered;
+}
+
+foreach (var update in unordered)
+{
+    isOrdered = CheckOrder(update, rules);
+
+    while (!isOrdered)    
+    {
+        isOrdered = CheckOrder(update, rules);
+    }
+}
+
+foreach (var update in unordered)
+{
+    acc += update[update.Count/2 ];
+}
+
+
+Console.WriteLine($"Part2: {acc} {acc == 4077}");
---
+++ day06/Program.cs
@@ -0,0 +1,168 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, Day 6!");
+
+
+var input = File.ReadAllLines("data/input.txt");
+
+var guard = new HashSet<char> {'^', 'v', '<', '>'};
+
+var visited = new Dictionary<(int y, int x), int>();
+
+var turns = new List<(int y, int x)>();
+
+(int, int) FindGuard(char[][] board)
+{
+    for (var y = 0; y < board.Length; y++)
+    {
+        for (var x = 0; x < board[y].Length; x++)
+        {
+            if (guard.Contains(board[y][x])) return (y, x);
+        }
+    }    
+
+    return (-1, -1);
+}
+
+void MakeTurn(char[][] board, (int y, int x) location)
+{
+    var turn = new Dictionary<char, char>()
+    {
+        {'^', '>'},
+        {'v', '<'},
+        {'<', '^'},
+        {'>', 'v'}
+    };
+
+    // make a turn
+    if (turns.Count == 0 || !(location == turns[turns.Count - 1])) turns.Add(location);
+
+    // change direction
+    board[location.y][location.x] = turn[board[location.y][location.x]];
+
+}
+
+void MarkVisited(int y, int x, char[][] board, bool track)
+{
+    
+        board[y][x] = 'X';
+        if (track && !visited.ContainsKey((y, x))) visited[(y, x)] = 1;
+        if (track) visited[(y, x)]++;
+        
+}
+
+(int, int) Move((int y, int x) guard, char[][] board, bool track = true)
+{
+    var nextStep = new Dictionary<char, (int, int)>()
+    {
+        {'^', (-1, 0)},
+        {'v',(1, 0)},
+        {'<', (0, -1)},
+        {'>', (0, 1)}
+    };
+
+    var (y, x) = guard;
+
+    // get the next step
+    var (moveY, moveX) = nextStep[board[y][x]];
+
+    //are we at the edge of the board?
+    if (y + moveY < 0 || x + moveX < 0 || y + moveY >= board.Length || x + moveX >= board[y].Length)
+    {
+        
+        MarkVisited(y, x, board, track);
+        return (-1, -1);
+    } 
+    
+    // keep going forward if no wall
+    if (board[y + moveY][x + moveX] != '#' && board[y + moveY][x + moveX] != 'O')
+    {
+        board[y+moveY][x+moveX] = board[y][x];
+        
+        MarkVisited(y, x, board, track);
+        
+        return (y + moveY, x + moveX);
+    }
+    
+
+    MakeTurn(board, (y, x));
+    
+    // and move too
+    return Move((y, x), board, track);
+
+}
+
+void PrintBoard(char[][] board)
+{
+    if (board.Length > 20) return;
+
+    for (var y = 0; y < board.Length; y++)
+    {
+        for (var x = 0; x < board[y].Length; x++)
+        {
+            Console.Write(board[y][x]);
+        }
+        Console.WriteLine();
+    }
+}
+
+var board = input
+    .Select(line => line.ToCharArray())
+    .ToArray();
+
+(int y, int x) = FindGuard(board);
+
+while (y != -1 && x != -1)
+{
+    (y, x) = Move((y, x), board);
+
+}
+
+Console.WriteLine($"Part1: unique locations: {visited.Count}");
+
+bool HasCycle(List<(int y, int x)> turns)
+{
+    // check if there are any duplicate turns
+
+    return turns.FindAll(t => t == turns[turns.Count - 1]).Count > 1;
+
+    // return turns.GroupBy(t => t)
+    //     .Where(g => g.Count() > 1).Any();
+}
+
+// part 2
+
+var cycles = 0;
+
+// printBoard(board);
+
+foreach (var visit in visited)
+{
+    board = input
+        .Select(line => line.ToCharArray())
+        .ToArray();
+
+    (y, x) = FindGuard(board);
+
+    if ((visit.Key.y, visit.Key.x) == (y, x)) continue;
+
+    board[visit.Key.y][visit.Key.x] = 'O';
+
+    turns.Clear();
+
+    bool done = false;
+
+    while (!done)
+    {
+        (y, x) = Move((y, x), board, false);
+
+        if (HasCycle(turns))
+        {
+            done = true;
+            cycles++;
+        }
+        else if (y == -1 || x == -1) done = true;
+    }
+}
+
+Console.WriteLine($"Part2: cycles: {cycles} ");
