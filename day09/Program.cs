b4c1a6e6aaaa74400c81e0d1c38e7cf3766dba98
kharshaw <keith.harshaw@gmail.com>
2024-12-10 20:43:07 -0500

    day09 (i forgot to commit yesterday) and day10

---
+++ day09/Program.cs
@@ -0,0 +1,153 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+using System.Text;
+using Microsoft.VisualBasic;
+
+Console.WriteLine("Hello, Day 9!");
+
+
+
+
+var input = File.ReadAllLines("data/input.txt");
+
+var condensed = input[0];
+
+var id = 0;
+var blocks = new List<int>();
+var files = new List<(int, int)>();
+
+for (int sizeIdx = 0, freeIdx = 1; sizeIdx < condensed.Length; sizeIdx += 2, freeIdx += 2)
+{
+
+    var size = int.Parse(condensed[sizeIdx].ToString());
+
+    var free = freeIdx < condensed.Length ? int.Parse(condensed[freeIdx].ToString()) : 0;
+
+    for (var i = 0; i < size; i++)
+    {
+        blocks.Add(id);
+    }
+
+    files.Add((id, size));
+
+    for (var i = 0; i < free; i++)
+    {
+        blocks.Add(-1);
+    }
+
+    files.Add((-1, free));
+
+    id++;
+}
+
+// compact blocks
+for (var i = blocks.Count - 1; i > 0; i--)
+{
+    var move = blocks[i];
+
+    if (move == -1) continue;
+
+    blocks[i] = -1;
+
+    for (var j = 0; j < blocks.Count; j++)
+    {
+        if (blocks[j] == -1) 
+        {
+            blocks[j] = move;
+            break;
+        }
+    }
+}
+
+long checksum = 0;
+for (var i = 0; i < blocks.Count; i++)
+{
+    if (blocks[i] == -1) continue;
+    checksum += i * blocks[i];
+}
+
+Console.WriteLine($"Part1: checksum: {checksum}");
+
+
+// part 2
+// PrintFilesystem(files);
+
+for (var fId = id - 1; fId > 0; fId--)
+{
+    var idx = files.FindIndex(x => x.Item1 == fId);
+    var moveFile = files[idx];
+
+
+    // Console.WriteLine($"Moving {moveFile.Item1} {moveFile.Item2}");
+    // find free space
+    for (var j = 0; j < files.Count; j++)
+    {
+        var freespace = files[j];
+
+    
+        // don't move past myself...
+        if (fId == freespace.Item1) break;
+   
+        // if this isn't free space then skip
+        if (freespace.Item1 != -1) continue;
+
+
+        // do we have space for this file
+        if (freespace.Item2 >= moveFile.Item2)
+        {
+            var free = freespace.Item2;
+            
+            // put file in free space
+            files[j] = moveFile;
+            
+            // if we have remaining free space, add it
+            if (free - moveFile.Item2 > 0)
+            {
+                files.Insert(j+1, (-1, free - moveFile.Item2));
+                files[idx+1] = (-1, moveFile.Item2);
+            }
+            else
+            {
+                files[idx] = (-1, moveFile.Item2);
+            }
+            // PrintFilesystem(files);
+            break;
+        }        
+    }
+}
+
+PrintFilesystem(files);
+
+// 00...111...2...333.44.5555.6666.777.888899
+
+// 00992111777.44.333....5555.6666.....8888..
+
+checksum = 0;
+var position = 0;
+
+foreach (var file in files)
+{
+    
+    for (var i = 0; i < file.Item2; i++)
+    {
+        if (file.Item1 != -1) checksum += position * file.Item1;
+        position++;
+    }
+
+}
+
+Console.WriteLine($"Part2: checksum: {checksum}");
+
+static void PrintFilesystem(List<(int, int)> files)
+{
+    // print files
+    foreach (var file in files)
+    {
+        for (var i = 0; i < file.Item2; i++)
+        {
+            Console.Write("{0}", file.Item1 == -1 ? "." : file.Item1);
+        }
+    }
+
+    Console.WriteLine();
+}
---
+++ day10/Program.cs
@@ -0,0 +1,124 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Day 10!");
+
+
+var input = File.ReadAllLines("data/input.txt");
+
+
+// create a two dimensional array of ints from input
+var board = input
+    .Select(line => line.ToCharArray().Select(i => int.Parse(i.ToString())).ToArray())
+    .ToArray();
+
+// printBoard(board);
+
+var acc = 0;
+var rating = 0;
+
+List<(int y, int x)> trailheads = FindTrailheads(board);
+
+foreach (var trailhead in trailheads)
+{
+    var (y, x) = trailhead;
+
+    var trails = HikeTrail(board, trailhead);
+
+    var trailends = new List<(int y, int x)>();
+
+    var score = trails.Select(t => t[0]).Distinct().Count();
+
+    acc += score;
+    rating += trails.Count();
+
+
+    //Console.WriteLine($"Trail: ({y},{x}) {score}");
+    // foreach (var trail in trails)
+    // {
+    //     foreach (var step in trail)
+    //     {
+    //         Console.Write($"({step.y}, {step.x}) ");
+    //     }
+    //     Console.WriteLine();
+    // }
+}
+
+Console.WriteLine($"Part1: {acc} ");
+
+Console.WriteLine($"Part2: {rating} ");
+
+List<List<(int y, int x)>> HikeTrail(int[][] board, (int y, int x) location)
+{
+    var trails = new List<List<(int y, int x)>>();
+
+    var locationHeight = board[location.y][location.x];
+
+    if (locationHeight == 9) 
+    {
+        trails.Add(new List<(int y, int x)>() {location});
+        return trails;
+    }    
+
+    var nextSteps = new List<(int y, int x)>();
+
+    var north = location.y - 1;
+    var south = location.y + 1;
+    var east = location.x + 1;
+    var west = location.x - 1;
+
+    // check north
+    if (north >= 0 && board[north][location.x] == locationHeight + 1) nextSteps.Add((north, location.x));
+
+    // check south
+    if (south < board.Length && board[south][location.x] == locationHeight + 1) nextSteps.Add((south, location.x));
+
+    // check east
+    if (east < board[0].Length && board[location.y][east] == locationHeight + 1) nextSteps.Add((location.y, east));
+
+    // check west        
+    if (west >= 0 && board[location.y][west] == locationHeight + 1) nextSteps.Add((location.y, west));
+
+    foreach (var nextStep in nextSteps)
+    {
+
+        var subtrails = HikeTrail(board, nextStep);
+
+        foreach (var trail in subtrails)
+        {
+            trail.Add(location);
+            trails.Add(trail);
+        }
+    }
+
+    return trails;
+
+}
+
+
+
+
+void printBoard(int[][] board)
+{
+    for (var y = 0; y < board.Length; y++)
+    {
+        for (var x = 0; x < board[y].Length; x++)
+        {
+            Console.Write("{0}", board[y][x]);
+        }
+        Console.WriteLine();
+    }
+}
+
+static List<(int y, int x)> FindTrailheads(int[][] board)
+{
+    var trailheads = new List<(int y, int x)>();
+
+    for (var y = 0; y < board.Length; y++)
+    {
+        for (var x = 0; x < board[y].Length; x++)
+        {
+            if (board[y][x] == 0) trailheads.Add((y, x));
+        }
+    }
+
+    return trailheads;
+}
